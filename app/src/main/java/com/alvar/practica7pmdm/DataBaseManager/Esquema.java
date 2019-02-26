c6bec2ee2b60dea0f5226804f501f3afc3e13456
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-02-26 09:14:47 +0100

    Realizando Base de datos close #4

---
+++ app/src/main/java/com/alvar/practica7pmdm/DataBaseManager/Esquema.java
@@ -0,0 +1,31 @@
+package com.alvar.practica7pmdm.DataBaseManager;
+
+import android.provider.BaseColumns;
+
+public class Esquema {
+
+    public Esquema() {
+    }
+
+    public static abstract class Lugar implements BaseColumns {
+        public static final String TABLE_NAME = "LUGAR";
+
+        public static final String COLUMN_NAME_ID = "id";
+        public static final String COLUMN_NAME_LATITUD = "latitud";
+        public static final String COLUMN_NAME_LONGITUD = "longitud";
+        public static final String COLUMN_NAME_NOMBRE = "nombre";
+        public static final String COLUMN_NAME_COMENTARIOS = "comentarios";
+        public static final String COLUMN_NAME_VALORACION = "valoracion";
+        public static final String COLUMN_NAME_CATEGORIA = "categoria";
+
+        public static final String COLUMN_TYPE_ID = "INTEGER";
+        public static final String COLUMN_TYPE_LATITUD = "FLOAT";
+        public static final String COLUMN_TYPE_LONGITUD = "FLOAT";
+        public static final String COLUMN_TYPE_NOMBRE = "TEXT";
+        public static final String COLUMN_TYPE_COMENTARIOS = "INTEGER";
+        public static final String COLUMN_TYPE_VALORACION = "FLOAT";
+        public static final String COLUMN_TYPE_CATEGORIA = "INT";
+
+    }
+
+}
