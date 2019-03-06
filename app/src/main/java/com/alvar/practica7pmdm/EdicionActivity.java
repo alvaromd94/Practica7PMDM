8d3e5321e4e3fb5c3ed3f6a677c5f3cd056d9bcd
alvaromd94 <37107484+alvaromd94@users.noreply.github.com>
2019-03-06 17:12:06 +0100

    Creando menu activity información close #34

--- app/src/main/java/com/alvar/practica7pmdm/EdicionActivity.java
+++ app/src/main/java/com/alvar/practica7pmdm/EdicionActivity.java
@@ -2,6 +2,7 @@ package com.alvar.practica7pmdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class EdicionActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class EdicionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edicion);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_edicion, menu);
+        return true;
     }
 }
--- app/src/main/java/com/alvar/practica7pmdm/InformacionActivity.java
+++ app/src/main/java/com/alvar/practica7pmdm/InformacionActivity.java
@@ -2,6 +2,7 @@ package com.alvar.practica7pmdm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class InformacionActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class InformacionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informacion);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_informacion, menu);
+        return true;
     }
 }
